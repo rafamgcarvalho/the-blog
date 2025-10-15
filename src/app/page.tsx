f1b23e8126a6202410809160044c00ce14124556
Rafael Carvalho <rafamgcarvalho@outlook.com>
2025-10-15 15:14:02 -0300

    header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,48 @@
+import { Container } from "@/components/Container";
+import { Header } from "@/components/Header";
 import { PostsList } from "@/components/PostsList";
 import { SpinLoader } from "@/components/SpinLoader";
-import { postRepository } from "@/repositories/post";
+import clsx from "clsx";
+import Image from "next/image"
+import Link from "next/link";
 import { Suspense } from 'react';
 
 export default async function HomePage() {
-  const posts = await postRepository.findAll();
-
   return (
-    <div>
-      <header>
-        <h1 className='text-6xl font-bold text-center py-8'>Aqui é a HEADER</h1>
-      </header>
+    <Container>
+      <Header></Header>
+
+      <section className={clsx(
+        "grid grid-cols-1 gap-8 mb-16",
+        "sm:grid-cols-2",
+        "group"
+        )}>
+        <Link className="w-full h-full overflow-hidden rounded-xl" href="#">
+          <Image
+            className="w-full h-full object-cover object-center group-hover:scale-105 transition"
+            src="/images/bryen_0.png"
+            width={1200}
+            height={720}
+            alt="Título"
+            priority>
+          </Image>
+        </Link>
+
+        <div className="flex flex-col gap-4 sm:justify-center">
+          <time className="text-slate-600 text-sm/tight" dateTime="2025-10-15">15/10/2025 15:00</time>
+
+          <h1 className="text-2xl/tight font-extrabold sm:text-4xl">
+            <Link href="#">Lorem ipsum dolor sit amet</Link>
+          </h1>
+
+          <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. In
+          voluptatem hic sit ea! Minus accusamus delectus eum alias
+          voluptates a aut, repellendus optio nostrum rem doloremque
+          pariatur consequatur eaque enim!
+          </p>
+        </div>
+      </section>
 
       <Suspense fallback={<SpinLoader />}>
         <PostsList />
@@ -19,6 +51,6 @@ export default async function HomePage() {
       <footer>
         <p className='text-6xl font-bold text-center py-8'>Footer</p>
       </footer>
-    </div>
+    </Container>
   );
 }
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,20 +1,17 @@
-'use client';
-
 import clsx from "clsx";
+import Link from "next/link";
 
 export function Header() {
   return (
-    <h1 className={clsx(
-      'text-xl',
-      'font-bold',
-      'text-blue-500',
-      'hover:text-blue-50',
-      'transition',
-      'duration-1000'
-    )}
-      onClick={() => alert(123)}
-    >
-      Texto do meu h1
-    </h1>
+    <header>
+      <h1 className={clsx(
+        "text-4xl/normal font-extrabold py-8",
+        "sm:text-5xl/normal sm:py-10",
+        "md:text-6xl/normal md:py-11",
+        "lg:text-7xl/normal lg:py-12",
+        )}>
+        <Link href="#">The Blog</Link>
+      </h1>
+    </header>
   )
 }

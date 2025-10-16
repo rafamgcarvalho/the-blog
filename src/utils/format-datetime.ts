6e5aa0f9c46a2544fb8ad21f345633b3b6714355
Rafael Carvalho <rafamgcarvalho@outlook.com>
2025-10-16 10:57:02 -0300

    Formatando datas com date-fns usando format e formatDistanceToNow

---
+++ src/utils/format-datetime.ts
@@ -0,0 +1,23 @@
+import { format, formatDistanceToNow } from "date-fns";
+import { ptBR } from "date-fns/locale";
+
+export function formatDateTime(rawDate: string): string {
+  const date = new Date(rawDate);
+
+  return format(date, "dd/MM/yyyy 'às' HH'h'mm", {
+    locale: ptBR,
+  });
+}
+
+export function formatRelativeDate(rawDate: string): string {
+  const date = new Date(rawDate);
+
+  return formatDistanceToNow(date, {
+    locale: ptBR,
+    addSuffix: true,
+  });
+}
+
+const rawDate = "2025-10-16T06:31:23.411Z"
+console.log(rawDate);
+console.log(formatRelativeDate(rawDate))

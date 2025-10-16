57002e875e8b2524e32b6eef95323ca8f1c55be9
Rafael Carvalho <rafamgcarvalho@outlook.com>
2025-10-16 08:44:12 -0300

    componente da imagem das postagens

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,9 @@
 import { Container } from "@/components/Container";
 import { Header } from "@/components/Header";
+import { PostCoverImage } from "@/components/PostCoverImage";
 import { PostHeading } from "@/components/PostHeading";
 import { PostsList } from "@/components/PostsList";
 import { SpinLoader } from "@/components/SpinLoader";
-import clsx from "clsx";
-import Image from "next/image"
-import Link from "next/link";
 import { Suspense } from 'react';
 
 export default async function HomePage() {
@@ -13,21 +11,20 @@ export default async function HomePage() {
     <Container>
       <Header></Header>
 
-      <section className={clsx(
-        "grid grid-cols-1 gap-8 mb-16",
-        "sm:grid-cols-2",
-        "group"
-        )}>
-        <Link className="w-full h-full overflow-hidden rounded-xl" href="#">
-          <Image
-            className="w-full h-full object-cover object-center group-hover:scale-105 transition"
-            src="/images/bryen_0.png"
-            width={1200}
-            height={720}
-            alt="Título"
-            priority>
-          </Image>
-        </Link>
+      <section className={"grid grid-cols-1 gap-8 mb-16 sm:grid-cols-2 group"}>
+
+        <PostCoverImage
+          linkProps={{
+            href: "#",
+          }}
+          imageProps={{
+            width: 1200,
+            height: 720,
+            src: "/images/bryen_9.png",
+            alt: "Titulo",
+            priority: true,
+          }}
+        ></PostCoverImage>
 
         <div className="flex flex-col gap-4 sm:justify-center">
           <time className="text-slate-600 text-sm/tight" dateTime="2025-10-15">15/10/2025 15:00</time>

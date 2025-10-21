0e2be1ec24bba174c041dc225502ef66f38f88c2
Rafael Carvalho <rafamgcarvalho@outlook.com>
2025-10-21 17:53:50 -0300

    Comportamento do Suspense e Loading com e sem Cache no Next.js

--- src/repositories/post/drizzle-post-repository.ts
+++ src/repositories/post/drizzle-post-repository.ts
@@ -2,9 +2,12 @@ import { PostModel } from "@/models/post/post-model";
 import { PostRepository } from "./post-repository";
 import { drizzleDb } from "@/db/drizzle";
 import { logColor } from "@/utils/log-color";
+import { asyncDelay } from "@/utils/async-delay";
+import { SIMULATE_WAIT_IN_MS } from "@/lib/constants";
 
 export class DrizzlePostRepository implements PostRepository {
     async findAllPublic(): Promise<PostModel[]> {
+      await asyncDelay(SIMULATE_WAIT_IN_MS, true);
       logColor("findAllPublic", Date.now());
 
       const posts = await drizzleDb.query.posts.findMany({
@@ -16,6 +19,8 @@ export class DrizzlePostRepository implements PostRepository {
     }
 
     async findBySlugPublic(slug: string): Promise<PostModel > {
+      await asyncDelay(SIMULATE_WAIT_IN_MS, true);
+
       logColor("findBySlugPublic", Date.now());
 
       const post = await drizzleDb.query.posts.findFirst({
@@ -28,6 +33,8 @@ export class DrizzlePostRepository implements PostRepository {
     }
 
     async findAll(): Promise<PostModel[]> {
+      await asyncDelay(SIMULATE_WAIT_IN_MS, true);
+
       logColor("findAll", Date.now());
 
       const posts = await drizzleDb.query.posts.findMany({
@@ -38,6 +45,8 @@ export class DrizzlePostRepository implements PostRepository {
     }
 
     async findById(id: string): Promise<PostModel> {
+      await asyncDelay(SIMULATE_WAIT_IN_MS, true);
+      
       logColor("findById", Date.now());
 
       const post = await drizzleDb.query.posts.findFirst({
--- src/repositories/post/json-post-repository.ts
+++ src/repositories/post/json-post-repository.ts
@@ -2,11 +2,10 @@ import { PostModel } from "@/models/post/post-model";
 import { PostRepository } from "./post-repository";
 import { resolve } from "path";
 import { readFile } from 'fs/promises';
+import { SIMULATE_WAIT_IN_MS } from "@/lib/constants";
 
 const ROOT_DIR = process.cwd();
 const JSON_POSTS_FILE_PATH = resolve(ROOT_DIR, 'src', 'db', 'seed', 'posts.json');
-
-const SIMULATE_WAIT_IN_MS = 0;
 
 export class JsonPostRepository implements PostRepository {
   private async simulateWait() {

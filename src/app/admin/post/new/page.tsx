55be4befdc8ea3e521174b61cb7c479495b4fa7b
Rafael Carvalho <rafamgcarvalho@outlook.com>
2025-10-25 11:07:52 -0300

    Formulário de criação e edição do post

--- src/app/admin/post/new/page.tsx
+++ src/app/admin/post/new/page.tsx
@@ -1,29 +1,11 @@
-import { Button } from "@/components/Button";
-import { InputCheckbox } from "@/components/InputCheckbox";
-import { InputText } from "@/components/InputText";
+import { ManagePostForm } from "@/components/admin/ManagePostForm";
 
 export const dynamic = "force-dynamic";
 
 export default async function AdminPostNewPage() {
   return (
-    <form action="" className="mb-16">
-      <div className="flex flex-col gap-6">
-        <InputText labelText="Nome" placeholder="Digite seu nome" />
-        <InputText labelText="Sobrenome" placeholder="Digite seu sobrenome" />
-        <InputText disabled labelText="Nome" placeholder="Digite seu nome" />
-
-        <InputCheckbox labelText="Sobrenome"></InputCheckbox>
-
-        <InputText labelText="Nome" placeholder="Digite seu nome" />
-        <InputText labelText="Nome" placeholder="Digite seu nome" />
-        <InputText labelText="Nome" placeholder="Digite seu nome" />
-
-        <div className="mt-4">
-          <Button type="submit">
-            Enviar
-          </Button>
-        </div>
-      </div>
-    </form>
+    <>
+    <ManagePostForm></ManagePostForm>
+    </>
   );
 }

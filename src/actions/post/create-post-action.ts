9c77e3f96d016d8050c4b36c283dc2e627b0bd37
Rafael Carvalho <rafamgcarvalho@outlook.com>
2025-10-27 10:34:06 -0300

    Zod para validação dos dados do FormData para criar e atualizar um post

--- src/actions/post/create-post-action.ts
+++ src/actions/post/create-post-action.ts
@@ -1,6 +1,11 @@
 "use server";
 
-import { PublicPost } from "@/dto/post/dto";
+import { makePartialPublicPost, PublicPost } from "@/dto/post/dto";
+import { PostCreateSchema } from "@/lib/post/validations";
+import { PostModel } from "@/models/post/post-model";
+import { getZodErrorMessages } from "@/utils/get-zod-error-messages";
+import { makeSlugFromText } from "@/utils/make-slug-from-text";
+import {v4 as uuidV4} from "uuid";
 
 type CreatePostActionState = {
   formState: PublicPost;
@@ -12,10 +17,37 @@ export async function createPostAction(
   formData: FormData,
 ): Promise<CreatePostActionState> {
 
-  const title = formData.get("title")?.toString() || "";
+  //TODO: verificar se o usuário está logado
+
+  if (!(formData instanceof FormData)) {
+    return {
+      formState: prevState.formState,
+      errors: ["Dados inválidos"],
+    }
+  }
+
+  const formDataToObj = Object.fromEntries(formData.entries()); //Pesquisar como usar entries
+  const zodParsedObj = PostCreateSchema.safeParse(formDataToObj);
+
+  if(!zodParsedObj.success) {
+    const errors = getZodErrorMessages(zodParsedObj.error.format());
+    return {
+      errors,
+      formState: makePartialPublicPost(formDataToObj),
+    }
+  }
+
+  const validPostData = zodParsedObj.data;
+  const newPost: PostModel = {
+    ...validPostData,
+    createdAt: new Date().toISOString(),
+    updatedAt: new Date().toISOString(),
+    id: uuidV4(),
+    slug: makeSlugFromText(validPostData.title),
+  };
 
   return {
-    formState: {...prevState.formState, title},
+    formState: newPost,
     errors: [],
   }
 }

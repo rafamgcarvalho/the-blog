e0dd11a81d9daa783587bc9b118e4980cc899b1b
Rafael Carvalho <rafamgcarvalho@outlook.com>
2025-10-27 11:11:23 -0300

    Salvando o novo post na base de dados e configurando images.remotePatterns

--- src/actions/post/delete-post-action.ts
+++ src/actions/post/delete-post-action.ts
@@ -32,7 +32,6 @@ export async function deletePostAction(id: string) {
   //TODO: mover esse método para o repositório
   await drizzleDb.delete(postsTable).where(eq(postsTable.id, id));
 
-  //TODO: revalidateTag ou revalidatePath
   revalidateTag("posts");
   revalidateTag(`post-${post.slug}`);
 

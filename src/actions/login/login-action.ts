115b95bbe1bb4a595468b5e200d342ac37589f3a
Rafael Carvalho <rafamgcarvalho@outlook.com>
2025-10-28 15:51:18 -0300

    Cookies no Next.js

--- src/actions/login/login-action.ts
+++ src/actions/login/login-action.ts
@@ -1,6 +1,8 @@
 "use server";
 
+import { createLoginSession, verifyPassword } from "@/lib/login/manage-login";
 import { asyncDelay } from "@/utils/async-delay";
+import { redirect } from "next/navigation";
 
 type LoginActionState = {
   username: string;
@@ -10,8 +12,35 @@ type LoginActionState = {
 export async function loginAction(state: LoginActionState, formData: FormData) {
   await asyncDelay(5000);
 
-  return {
-    username: "",
-    error: "",
+  if (!(formData instanceof FormData)) {
+    return {
+      username: "",
+      error: "Dados inválidos"
+    }
   }
+
+  //Dados que o usuário digitou no form
+  const username = formData.get("username")?.toString().trim() || "";
+  const password = formData.get("password")?.toString().trim() || "";
+
+  if (!username || !password) {
+    return {
+      username,
+      error: "Digite o usuário e a senha",
+    }
+  }
+
+  //Checaria se o usuário existe na base de dados
+  const isUsernameValid = username === process.env.LOGIN_USER;
+  const isPasswordValid = await verifyPassword(password, process.env.LOGIN_PASS || "");
+
+  if (!isUsernameValid || !isPasswordValid) {
+    return {
+      username,
+      error: "Usuário ou senha inválidos",
+    }
+  }
+
+  await createLoginSession(username);
+  redirect("/admin/post");
 }
